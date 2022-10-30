77a7137db25ddef3ac8a04735e8c2320e34a46f4
Guy Allen <gallen@ukcloud.com>
2022-10-30 17:05:33 +0000

    fix: add entry to .gitignore and lint py. file

--- calculator.py
+++ calculator.py
@@ -2,8 +2,10 @@
 
 # Calcuator library containing basic math operations
 
+
 def add(first_term, second_term):
     return first_term + second_term
+
 
 def subtract(first_term, second_term):
     return first_term - second_term
